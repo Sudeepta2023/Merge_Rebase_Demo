64050e122889928daeba09c3a7be8a387518341b
Sudeepta Kumar Pradhan <od0346sudeepta@gmail.com>
2023-04-30 01:34:01 +0530

    hi

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ let a= 90 ;
 let b= 45 ;
 console.log("sum of two number is - "+ (a+b));
 console.log(a+b);
-//  changes by user-1  //
-let mul = Number(a)*Number(b);
-console.log("mul of two number is - " +  (a*b));
-console.log(a*b)
+
 
