771f87c7e9640ff80d7dd81d888c310139358d94
Sudeepta Kumar Pradhan <od0346sudeepta@gmail.com>
2023-04-30 01:37:46 +0530

    added by the user two after new branch creation

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ let a= 90 ;
 let b= 45 ;
 console.log("sum of two number is - "+ (a+b));
 console.log(a+b);
-
+//  changes by user-1  //
+let mul = Number(a)*Number(b);
+console.log("mul of two number is - " +  (a*b));
+console.log(a*b);
 
