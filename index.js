219c948388a0508d7738ed16f562e268a4592480
Sudeepta Kumar Pradhan <od0346sudeepta@gmail.com>
2023-04-30 01:57:44 +0530

    feature_1

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ let a= 90 ;
 let b= 45 ;
 console.log("sum of two number is - "+ (a+b));
 console.log(a+b);
+<<<<<<< HEAD
 //----2 nd operation add in main---//
 console.log("sub of two numbers - " +(a-b));
 
+=======
+//  changes by user-1  //
+let mul = Number(a)*Number(b);
+console.log("mul of two number is - " +  (a*b));
+console.log(a*b);
+>>>>>>> 771f87c (added by the user two after new branch creation)
 
 console.log("Div of two number is - " + (a/b));
